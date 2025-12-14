a16c43a453b6c0ad77b21325ee0c48d0abf6a171
= <=>
2025-12-14 17:06:59 +0530

    created a submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import "./../styles/App.css";
-import TodoList from "./TodoList";
 
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, text: "Learn React", completed: false }
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a React App", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false }
   ]);
 
   const handleComplete = (id) => {
     setTodos(
-      todos.map(todo =>
+      todos.map((todo) =>
         todo.id === id ? { ...todo, completed: true } : todo
       )
     );
@@ -19,7 +20,20 @@ const App = () => {
     <div>
       {/* Do not remove the main div */}
       <h1>Todo App</h1>
-      <TodoList todos={todos} handleComplete={handleComplete} />
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
